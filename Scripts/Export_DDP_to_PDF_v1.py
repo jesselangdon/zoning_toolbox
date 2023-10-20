0ca0576669077fcd02d3e1fddba18a0e27a4229c
Langdon <SCDJ2L@co.snohomish.wa.us>
2023-10-19 17:05:06 -0700

    Refactored to allow export to section maps, or full TWP

--- Scripts/Export_DDP_to_PDF_v1.py
+++ Scripts/Export_DDP_to_PDF_v1.py
@@ -19,24 +19,34 @@ History:   20120329 - Final Debugging and moved into production
 import arcpy
 import os, os.path, datetime, glob
 
-#--Variables
-gpat_continue_cond = arcpy.GetParameterAsText(0)
-# gpat_continue = "true" #TEST
-# gpat_continue_cond = str(gpat_continue)
-aprx = arcpy.mp.ArcGISProject("CURRENT")
-# aprx = arcpy.mp.ArcGISProject(r'C:\Users\SCDJ2L\dev\Zoning\OZmapMaker_v0\OZmapMaker_v20230829.aprx') #TEST
+# Set global variables
+
+# gpat_layout = arcpy.GetParameterAsText(0)
+gpat_layout = "Full Township"
+# gpat_continue_cond = arcpy.GetParameterAsText(1)
+gpat_continue = "true" #TEST
+gpat_continue_cond = str(gpat_continue)
+# aprx = arcpy.mp.ArcGISProject("CURRENT")
+aprx = arcpy.mp.ArcGISProject(r'C:\Users\SCDJ2L\dev\Zoning\OZmapMaker_v0\OZmapMaker_FullTWP.aprx') #TEST
 map = aprx.listMaps("Premier Data Frame")[0]
 lyr = map.listLayers(".PageGrid_MASTERPageGrid")[0]
-layout = aprx.listLayouts()[1]
-if not layout.mapSeries is None:
-    ms = layout.mapSeries
-else:
-    arcpy.AddError("Map series not available in this Pro project! Map series must be created manually in Pro first...")
 
-currentdir = r'\\snoco\gis\plng\images\zoning_current\pdf'
-# currentdir = r"C:\Users\SCDJ2L\dev\Zoning\TEST\zoning_current\pdf" #TEST
-archivedir = r'\\snoco\Dept_Images\PDS\PDSHistoricalMapsandAerials\HistoricalZoningMaps'
-# archivedir = r"C:\Users\SCDJ2L\dev\Zoning\TEST\HistoricalZoningMaps\pdf" #TEST
+# currentdir = r'\\snoco\gis\plng\images\zoning_current\pdf'
+currentdir = r"C:\Users\SCDJ2L\dev\Zoning\TEST\zoning_current\pdf" #TEST
+# archivedir = r'\\snoco\Dept_Images\PDS\PDSHistoricalMapsandAerials\HistoricalZoningMaps'
+archivedir = r"C:\Users\SCDJ2L\dev\Zoning\TEST\HistoricalZoningMaps\pdf" #TEST
+
+
+def get_layout(user_aprx):
+    '''Returns a layout object from the APRX, only if it is one of the two valid layouts'''
+    layouts_valid = ["FULL_TWP_DDP_OZmap_v1", "TR_DDP_OZmap_v1"]
+    layouts_aprx = user_aprx.listLayouts()
+    for layout in layouts_aprx:
+        if layout.name in layouts_valid:
+            return layout
+        else:
+            arcpy.AddError(f"There are no valid layouts in {user_aprx.filePath}")
+
 
 def get_date_stamp():
     '''Generate date stamp suffix'''
@@ -57,14 +67,14 @@ def find_all(a_str, sub):
         start += len(sub)
 
 
-def ToDelCurPDF(CurPDF):
+def delete_previous_PDF(CurPDF):
     '''Deletes a file with a wildcard (i.e. the existing current .PDF, hence glob)'''
     for fl in glob.glob(CurPDF):
         os.remove(fl)
     return
 
 
-def ToRemoveExisting(ExistingPDF):
+def delete_duplicate_PDF(ExistingPDF):
     if os.path.exists(ExistingPDF):
         arcpy.AddMessage("Removing existing (i.e. duplicate) .PDF file: " + str(ExistingPDF))
         os.remove(ExistingPDF)
@@ -83,89 +93,118 @@ def isrunning(exe):
         return "None"
 
 
+def get_export_filename_section(mpg_parse_list):
+    '''Returns four string variables, if exporting PDF files by section'''
+    if len(str(mpg_parse_list[1])) == 1:  # section string - if 1-digit Section, then add "0"
+        a_sec = "0" + str(mpg_parse_list[1])
+    elif len(str(mpg_parse_list[1])) == 2:
+        a_sec = str(mpg_parse_list[1])
+
+    a_twp = str(mpg_parse_list[2][1:3])  # township string
+    if len(str(mpg_parse_list[3])) > 3:  # range string - if 2-digit range, then slice by [1:3]
+        a_rng = str(mpg_parse_list[3][1:3])
+    elif len(str(mpg_parse_list[3])) == 3:
+        a_rng = "0" + str(mpg_parse_list[3][1:2])
+
+    if len(str(mpg_parse_list[0])) == 3:  # quarter section string
+        a_qtr = str(0)
+    elif len(str(mpg_parse_list[0])) > 3:
+        a_qtr = str(5)
+    elif str(mpg_parse_list[0]) == "NE":
+        a_qtr = str(1)
+    elif str(mpg_parse_list[0]) == "NW":
+        a_qtr = str(2)
+    elif str(mpg_parse_list[0]) == "SW":
+        a_qtr = str(3)
+    elif str(mpg_parse_list[0]) == "SE":
+        a_qtr = str(4)
+
+    return a_twp, a_rng, a_sec, a_qtr
+
+
+def get_export_filename_fullTWP(parse_list):
+    '''Returns four string variables, if exporting PDF files by for the full towship'''
+    a_sec = r"00"                               # section string
+    a_twp = str(parse_list[0][1:3])             # township string
+    if len(str(parse_list[1])) > 3:             # range string - if 2-digit range, then slice by [1:3]
+        a_rng = str(parse_list[1][1:3])
+    elif len(str(parse_list[1])) == 3:
+        a_rng = "0" + str(parse_list[1][1:2])
+    a_qtr = str(0)                              # quarter section string
+
+    return a_twp, a_rng, a_sec, a_qtr
+
+
+def kill_acrobat():
+    # Taskkill to perform a hard close on Adobe Acrobat Reader
+    PID_acro32 = isrunning('AcroRd32.exe')
+    if PID_acro32 != "None":
+        os.system(r'taskkill /F /PID ' + PID_acro32)
+        arcpy.AddWarning("Adobe Acrobat Reader was terminated...")
+
+    # Taskkill to perform a hard close on Adobe Acrobat Pro
+    PID_acro = isrunning('Acrobat.exe')
+    if PID_acro != "None":
+        os.system(r'taskkill /F /PID ' + PID_acro)
+        arcpy.AddWarning("Adobe Acrobat Pro was terminated...")
+
+    return
+
+
 def ms_loop():
-    '''Loop through map series pages'''
-    #--Global variables
+    '''Loop through map series pages to export to PDF'''
+    # Global variables
     global currentdir, archivedir, currentpath, archivepath
 
-    #--While loop begins
+    # While loop begins
     pg = 1
     while pg <= ms.pageCount:
-        #--Change current page name
+        # Change current page name
         ms.currentPageNumber = pg
 
-        #--Get QSTR-name of current page from .MasterPageGrid
+        # Get QSTR-name of current page from .MasterPageGrid
         mpg_label = ms.pageRow.LABEL2
 
-        #--Split LABEL per empty spaces
-        mpg_parseList = mpg_label.split()
+        # Split QSTR-name label per empty spaces
+        mpg_parse_list = mpg_label.split()
 
-        #--Sectional
-        if len(str(mpg_parseList[1])) == 1:     #--Sec. Str - If... 1-digit Section, then add "0"
-            a_sec = "0" + str(mpg_parseList[1])
-        elif len(str(mpg_parseList[1])) == 2:
-            a_sec = str(mpg_parseList[1])       
-
-        a_twp = str(mpg_parseList[2][1:3])      #--Twp. Str
-
-        if len(str(mpg_parseList[3])) > 3:      #--Rng. Str - If... 2-digit Range, then slice by [1:3]
-            a_rng = str(mpg_parseList[3][1:3])
-        elif len(str(mpg_parseList[3])) == 3:
-            a_rng = "0" + str(mpg_parseList[3][1:2])
-
-        if len(str(mpg_parseList[0])) == 3:     #--Qtr. Str -
-            a_qtr = str(0)
-        elif len(str(mpg_parseList[0])) > 3:
-            a_qtr = str(5)
-        elif str(mpg_parseList[0]) == "NE":
-            a_qtr = str(1)
-        elif str(mpg_parseList[0]) == "NW":
-            a_qtr = str(2)        
-        elif str(mpg_parseList[0]) == "SW":
-            a_qtr = str(3)
-        elif str(mpg_parseList[0]) == "SE":
-            a_qtr = str(4)
+        # Get PLSS string which will be included in PDF export filename
+        if gpat_layout == "Full Township":
+            a_twp, a_rng, a_sec, a_qtr = get_export_filename_fullTWP(mpg_parse_list)
+        elif gpat_layout == "Individual Sections":
+            a_twp, a_rng, a_sec, a_qtr = get_export_filename_section(mpg_parse_list)
 
         arcpy.AddMessage("Section = {0}, Township = {1}, Range = {2}, Quarter Section = {3}".format(a_sec, a_twp, a_rng, a_qtr))
-        fname = a_twp + a_rng + a_sec + a_qtr + str(date_stamp) + ".pdf"
-        fname_cur = a_twp + a_rng + a_sec + a_qtr + "_*.pdf"
+        fname = f"{a_twp}{a_rng}{a_sec}{a_qtr}{str(date_stamp)}.pdf"
+        fname_cur = f"{a_twp}{a_rng}{a_sec}{a_qtr}.pdf"
         arcpy.AddMessage("Archived filename: {}".format(fname))
         arcpy.AddMessage("Current filename: {}".format(fname_cur))
         
-        #--Taskkill to perform a hard close on Adobe Acrobat Reader
-        PID = isrunning('AcroRd32.exe')
-        if PID != "None" :
-            os.system(r'taskkill /F /PID ' + PID)
-            
-        #--Taskkill to perform a hard close on Adobe Acrobat Pro
-        PID = isrunning('Acrobat.exe')
-        if PID != "None" :
-            os.system(r'taskkill /F /PID ' + PID)
+        kill_acrobat()
 
-        #--Remove existing .PDF with previous date from current folder (with wildcard, hence glob)
+        # Remove existing .PDF with previous date from current folder
         currentpath = os.path.join(currentdir, a_twp + a_rng, fname_cur)
+        delete_previous_PDF(currentpath)
 
-        ToDelCurPDF(currentpath)
-
-        #--Remove existing .PDF with same names from current and archive folders
-        currentpath = os.path.join(currentdir, a_twp + a_rng, fname)
+        # Remove existing .PDF with same names from current and archive folders
+        currentpath = os.path.join(currentdir, a_twp + a_rng, fname_cur)
         archivepath = os.path.join(archivedir, a_twp + a_rng, fname)
         arcpy.AddMessage("currentpath: " + currentpath)
         arcpy.AddMessage("archivepath: " + archivepath)
 
         arcpy.AddMessage("Removing existing PDF files from current and archive folders...")
-        ToRemoveExisting(currentpath)
-        ToRemoveExisting(archivepath)
+        delete_duplicate_PDF(currentpath)
+        delete_duplicate_PDF(archivepath)
 
-        #--Export .PDF to archive folder
+        # Export .PDF to archive folder
         arcpy.AddMessage("Exporting PDF to current folder...")
         layout.exportToPDF(currentpath)
 
-        #--Copy .PDF file from current to archive folder
+        # Copy .PDF file from current to archive folder
         arcpy.AddMessage("Copying to archive folder...")
         os.system("copy %s %s" % (currentpath, archivepath))
 
-        #--While loop increment page number
+        # While loop increment page number
         pg = pg + 1
 
     return
@@ -174,6 +213,17 @@ def ms_loop():
 #--Read the .MasterPageGrid definition query to label archive and current folder paths
 if gpat_continue_cond == "true":
     date_stamp = get_date_stamp()
+
+    # Get layout from the APRX project, only if it is one of two "valid" OZmap layouts
+    layout = get_layout(aprx)
+    arcpy.AddMessage(f"layout: {layout.name}")
+
+    if not layout.mapSeries is None:
+        ms = layout.mapSeries
+    else:
+        arcpy.AddError(
+            "Map series not available in this project's layout! Map series must be created manually in Pro first...")
+
     if lyr.supports("DEFINITIONQUERY"):
         # --Definition query of .PageGrid_MasterPageGrid
         dq = lyr.definitionQuery
@@ -184,8 +234,8 @@ if gpat_continue_cond == "true":
             dq_list = list(find_all(dq, '='))
             arcpy.AddMessage("dq_list: " + str(dq_list))
 
-            #--(WHILE) Loop through DDP pages
-            arcpy.AddMessage("Initiating WHILE loop through map series pages")
+            #--Loop through map series pages to export
+            arcpy.AddMessage("Initiating loop through map series pages")
             ms_loop()
 
         else:    #--Def. Qry. count
@@ -194,4 +244,4 @@ if gpat_continue_cond == "true":
     else:    #--if lyr.supports
         arcpy.AddError("The feature class does not support definition queries!")
 else:     #--if Continue checkbox not checked
-    arcpy.AddError("Unchecked parameter box ... Bailing out of script tool!")
+    arcpy.AddError("Unchecked parameter box ... Terminating OZ map export process!")
